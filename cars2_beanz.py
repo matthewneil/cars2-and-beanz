15065169a18e83ec90bb92f7bc3ae26c86a6f1bb
matthewneil <49290261+matthewneil@users.noreply.github.com>
2019-11-18 14:41:55 +1300

    Update cars2_beanz.py

--- cars2_beanz.py
+++ cars2_beanz.py
@@ -3,7 +3,6 @@ import requests
 
 print("Cars 2 4k Bluray Prices")
 
-#Getting Price of Cars 2 4K bluray from MightyApe
 source = requests.get('https://www.mightyape.co.nz/product/cars-2-4k-uhd-uhd-blu-ray/31420594').text
 soup = BeautifulSoup(source, 'lxml')
 
@@ -11,7 +10,6 @@ carsMA = soup.find('span', class_='price').text.strip()
 carsMA = carsMA.replace('$', '');
 print("MightyApe.co.nz:\t$", carsMA)
 
-#Getting Price of Cars 2 4K bluray from JB HiFi
 source = requests.get('https://www.jbhifi.co.nz/family/cars-2/372219/').text
 soup = BeautifulSoup(source, 'lxml')
 
@@ -20,18 +18,16 @@ carsJB = carsJB.replace(' ', '');
 carsJB = carsJB.replace('$', '');
 print("jbhifi.co.nz:\t\t$", carsJB)
 
-#Getting Heinz Beans Price from Countdown
 source = requests.get('https://shop.countdown.co.nz/shop/productdetails?stockcode=362231&name=heinz-baked-beans&searchString=beanz').text
 soup = BeautifulSoup(source, 'lxml')
 
 print("\nHeinz Beanz Prices")
 
-beanzCD = soup.find('span', class_='price din-medium').text.strip()
+beanzCD = soup.find('span', class_='club-price-wrapper club-text-colour din-medium').text.strip()
 beanzCD = beanzCD.replace('$', '');
 beanzCD = beanzCD.replace('ea', '').strip();
 print("countdown.co.nz:\t$", beanzCD)
 
-#Getting Heinz Beans Price from New World
 source = requests.get('https://www.ishopnewworld.co.nz/product/5012022_ea_000nw?name=beanz-english-recipe-baked-beans').text
 soup = BeautifulSoup(source, 'lxml')
 
@@ -40,7 +36,6 @@ beanzNW = beanzNW.replace('ea', '').strip();
 beanzNW = beanzNW.replace(' ', '.');
 print("newworld.co.nz:\t\t$", beanzNW)
 
-#Calculate the best conbination in price
 carsMA = float(carsMA)
 carsJB = float(carsJB)
 beanzCD = float(beanzCD)
